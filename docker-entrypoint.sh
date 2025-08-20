2b600ee30dadc51392a232fcb7bd76986dcdf24f
jpaulovaz <jpmcvs@gmail.com>
2025-08-20 09:41:48 -0300

    Primeiro Ajuste
    - Primeiro Ajuste

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ mkdir -p /app/config/kometa/tssk
 cp -r /app/files/* /app/config/
 
 # Ajustar permissões se necessário
-chown -R "${PUID}:${PGID}" /app/config
+chown -R "${PUID}:${PGID}" /app
 
 echo "$CRON cd /app && /usr/local/bin/python TSSK.py 2>&1 | tee -a /var/log/cron.log" > /etc/cron.d/tssk-cron
 chmod 0644 /etc/cron.d/tssk-cron
