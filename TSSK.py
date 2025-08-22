4225bca061e1a7ae0a1b442cd5a0e3fae5c69a0e
jpaulovaz <jpmcvs@gmail.com>
2025-08-22 01:57:40 -0300

    Teste de Depuração Docker
    - Depuração em produção.

--- TSSK.py
+++ TSSK.py
@@ -15,13 +15,14 @@ IS_DOCKER = os.getenv("DOCKER", "false").lower() == "true"
 VERSION = "1.2.0"
 
 
-
 if IS_DOCKER:
     os.makedirs("/app/config/kometa/tssk", exist_ok=True)
     puid = int(os.getenv("PUID", "1000"))
     pgid = int(os.getenv("PGID", "1000"))
     overlay_path = "/app/config/kometa/tssk/"
     collection_path = "/app/config/kometa/tssk/"
+
+print(f"Usuário: {puid} Grupo:{pgid} Em execução.")
 
 # ANSI color codes
 GREEN = '\033[32m'
